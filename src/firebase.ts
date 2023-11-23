779dccdf714fd7d429e127ff9cd6f94d97429af0
BRI <frametable@gmail.com>
2023-11-23 17:40:27 +0900

    v3

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4m8bkMEC-qvXbPtt9dl6Eugq7lGCPdxE",
+  authDomain: "nwitter-reloaded-f05f6.firebaseapp.com",
+  projectId: "nwitter-reloaded-f05f6",
+  storageBucket: "nwitter-reloaded-f05f6.appspot.com",
+  messagingSenderId: "529905550308",
+  appId: "1:529905550308:web:9502b8534acc30c7147df1",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
